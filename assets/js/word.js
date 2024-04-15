c8010cf26ccc954ec9a7a8523b3be2d03b257149
Ngozi Omenka <ngozi.madu@live.se>
2024-04-15 15:34:12 +0000

    Added wordlist for uses to guess

---
+++ assets/js/word.js
@@ -0,0 +1,198 @@
+const wordList = [
+    {
+        word: " greece",
+        hint: "a country in europe"
+    },
+    {
+        word: "goose",
+        hint: "a bird"
+    },
+    {
+        word: "bridge",
+        hint: "use to cross water"
+    },
+    {
+        word: "deputy",
+        hint: "second to a leader"
+    },
+    {
+        word: "sliver",
+        hint: "a shining precious metal"
+    },
+    {
+        word: "etsy",
+        hint: "popular online shopping site"
+    },
+    {
+        word: "kotlin",
+        hint: "a programming language"
+    },
+    {
+        word: "orange",
+        hint: "fruit, good for making juice"
+    },
+    {
+        word: "africa",
+        hint: "a continet"
+    },
+    {
+        word: "leg",
+        hint: "part of the body"
+    },
+    {
+        word: "seven",
+        hint: "a number"
+    },
+    {
+        word: "remote",
+        hint: "use to operate smart device"
+    },
+    {
+        word: "fish",
+        hint: "found in the river"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "sand",
+        hint: "something uncountable"
+    },
+    {
+        word: "tree",
+        hint: "found in bushes"
+    },
+    {
+        word: "prank",
+        hint: "mischievous act"
+    },
+    {
+        word: "wechat",
+        hint: "a social media app"
+    },
+    {
+        word: "birth",
+        hint: "to bring forth a new thing"
+    },
+    {
+        word: "cash",
+        hint: "use for exchange of goods and services"
+    },
+    {
+        word: "alert",
+        hint: "it notifies one of something"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "crime",
+        hint: "act of breaking laws"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "bing",
+        hint: "famous search engine"
+    },
+    {
+        word: "equal",
+        hint: "when mutiple things are the same"
+    },
+    {
+        word: "green",
+        hint: "a color"
+    },
+    {
+        word: "pillow",
+        hint: "use for sleeping"
+    },
+    {
+        word: "school",
+        hint: "a space for learning"
+    },
+    {
+        word: "speak",
+        hint: "a method of commuincation"
+    },
+    {
+        word: "bitcoin",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "toyota",
+        hint: "a car brand"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "jacket",
+        hint: "use during winter"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "pot",
+        hint: "use for cooking"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "makeup",
+        hint: "a cosmetics"
+    },
+    {
+        word: "castel",
+        hint: "a place to live comfortably"
+    },
+    {
+        word: "attack",
+        hint: "related to war"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "job",
+        hint: "to earn money"
+    },
+    {
+        word: "cancer",
+        hint: "a deadly sickness"
+    },
+    {
+        word: "egg",
+        hint: "can be eaten for breakfast"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
