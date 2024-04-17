52b4998975ebb9add23ba9a51137628211312668
Ngozi Omenka <ngozi.madu@live.se>
2024-04-17 18:28:46 +0000

    Added required elements and  activate start game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log()
+// Add all required elements
+const start_button = document.querySelector("#start_button");
+const infomation = document.querySelector("#infomation");
+const exit_btn = infomation.querySelector("#buttons");
+const continue_btn = infomation.querySelector("#buttons");
+const inputs = document.querySelector(".inputs")
+
+// Activate start game button
+start_button.onclick = ()=>{
+    infomation.classList.add("activeInfo");
+};
+
+exit_btn.onclick = ()=>{
+    infomation.classList.remove("activeInfo");
+}
+
+continue_btn.onclick = ()=>{
+    infomation.classList.remove("activeInfo");
+    guess-the-world.classList.add("activeQuess");
+    showQuestions(0);
+}
