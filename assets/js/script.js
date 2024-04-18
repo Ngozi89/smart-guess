52bf72a8070fa7d967c1bc89c97d81ccfaebb1f7
Ngozi Omenka <ngozi.madu@live.se>
2024-04-18 05:54:26 +0000

    Added all required elements and randomword fuctionality to enable questions display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,78 @@
 // Add all required elements
-const start_button = document.querySelector("#start_button");
-const infomation = document.querySelector("#infomation");
-const exit_btn = infomation.querySelector("#buttons");
-const continue_btn = infomation.querySelector("#buttons");
-const inputs = document.querySelector(".inputs")
+let start_game = document.querySelector(".start_game button");
+let game_info_list = document.querySelector(".game_info_list");
+let exit_btn = game_info_list.querySelector(".buttons .exit");
+let continue_btn = game_info_list.querySelector(".buttons .continue");
+let inputs = document.querySelector(".inputs"),
+
+hintTag = document.querySelector(".hint span"),
+guessLeft = document.querySelector(".guess-left span"),
+wrongLetter = document.querySelector(".wrong-letter span"),
+resetBtn = document.querySelector(".reset-btn"),
+typingInput = document.querySelector(".typing-input");
 
 // Activate start game button
-start_button.onclick = ()=>{
-    infomation.classList.add("activeInfo");
+start_game.onclick = ()=>{
+    game_info_list.classList.add("activeInfo");
 };
 
 exit_btn.onclick = ()=>{
-    infomation.classList.remove("activeInfo");
+    game_info_list.classList.remove("activeInfo");
 }
 
 continue_btn.onclick = ()=>{
-    infomation.classList.remove("activeInfo");
-    guess-the-world.classList.add("activeQuess");
+    game_info_list.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
     showQuestions(0);
 }
+
+let word, maxGuesses, incorrectLetters = [], correctLetters = [];
+function randomWord() {
+    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []; incorrectLetters = [];
+    hintTag.innerText = ranItem.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrectLetters;
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        inputs.innerHTML = html;
+    }
+}
+randomWord();
+function initGame(e) {
+    let key = e.target.value.toLowerCase();
+    if(key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
+        if(word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if(word[i] == key) {
+                    correctLetters += key;
+                    inputs.querySelectorAll("input")[i].value = key;
+                }
+            }
+        } else {
+            maxGuesses--;
+            incorrectLetters.push(` ${key}`);
+        }
+        guessLeft.innerText = maxGuesses;
+        wrongLetter.innerText = incorrectLetters;
+    }
+    typingInput.value = "";
+    setTimeout(() => {
+        if(correctLetters.length === word.length) {
+            alert(`Congrats! You found the word ${word.toUpperCase()}`);
+            return randomWord();
+        } else if(maxGuesses < 1) {
+            alert("Game over! You don't have remaining guesses");
+            for(let i = 0; i < word.length; i++) {
+                inputs.querySelectorAll("input")[i].value = word[i];
+            }
+        }
+    }, 100);
+}
+resetBtn.addEventListener("click", randomWord);
+typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
+document.addEventListener("keydown", () => typingInput.focus());
